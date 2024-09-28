374df7e627ab88d6c7fbbce87d44a937c82d241e
sanjay <sanju4232908@gmail.com>
2024-09-28 14:00:35 +0530

    contact us

--- src/modules/contactUs/sections/ServiceContactUs.tsx
+++ src/modules/contactUs/sections/ServiceContactUs.tsx
@@ -17,7 +17,7 @@ const ServiceContactUs: React.FC = () => {
               placeholder="Search Your Answers"
               className="px-2 m-2 lg:m-3 xl:px-4 xl:m-4 text-sm  xl:text-lg"
             />
-            <button className="mr-1 px-4 py-2 my-1  md:mx-1  md:my-1 md:px-4 md:py-2  lg:px-4 lg:p-2 xl:py-4 bg-black text-white text-sm xl:text-lg rounded-2xl xl:rounded-full">
+            <button className="mr-1 px-4 py-2 my-1  md:mx-1  md:my-1 md:px-4 md:py-2  lg:px-4 lg:py-2  xl:py-4 xl:px-6 bg-black text-white text-sm xl:text-lg rounded-2xl xl:rounded-full">
               Search
             </button>
           </div>
