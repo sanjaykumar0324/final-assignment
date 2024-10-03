c2f79583ecc29bfbadb384b9d9b748585922b3d7
sanjay <sanju4232908@gmail.com>
2024-10-03 11:07:46 +0530

    fetch locale file

--- src/modules/contactUs/sections/FAQ.tsx
+++ src/modules/contactUs/sections/FAQ.tsx
@@ -7,7 +7,7 @@ const FAQ: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
   useEffect(() => {
-    fetch("/data/faqData.json")
+    fetch("/src/data/faqData.json")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
